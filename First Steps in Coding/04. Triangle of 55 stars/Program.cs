66cd6082317a2a2e976b809d1149d3fa007c976d
TsvetomirNikolov <tsvetomir.nikolov1@abv.bg>
2017-03-13 21:41:34 +0200

    Update README.md

---
+++ First Steps in Coding/04. Triangle of 55 stars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(new string('*', i));
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ First Steps in Coding/06. Square of stars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', n));
+            for (int i = 1; i <= n - 2; i++)
+            {
+                Console.Write(new string('*', 1));
+                Console.Write(new string(' ', n - 2));
+                Console.Write(new string('*', 1));
+                Console.WriteLine();
+            }
+            Console.WriteLine(new string('*', n));
+        }
+    }
+}
